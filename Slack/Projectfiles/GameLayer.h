fc61fcaac2fcb7a612acd8f5ec2d8647e64de4ba
Jacob Preston <jacobpreston4@gmail.com>
2013-04-14 16:30:48 -0400

    Added Gamelayer files

---
+++ Slack/Projectfiles/GameLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameLayer.h
+//  Slack
+//
+//  Created by Jacob Preston on 4/14/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+    
+}
+
+@end
---
+++ Slack/Projectfiles/GameLayer.m
@@ -0,0 +1,14 @@
+//
+//  GameLayer.m
+//  Slack
+//
+//  Created by Jacob Preston on 4/14/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameLayer.h"
+
+
+@implementation GameLayer
+
+@end
