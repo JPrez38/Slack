48f7686817b838847a15be73ab201f5dd14ecefe
Jacob Preston <jacobpreston4@gmail.com>
2013-05-17 11:47:19 -0500

    fixed find out moe

--- Slack/Projectfiles/GetInvolvedLayer.m
+++ Slack/Projectfiles/GetInvolvedLayer.m
@@ -59,22 +59,34 @@
                                                          selectedImage: @"main_menu_icon2.png"
                                                                 target:self
                                                               selector:@selector(doSomething:)];
+    CCMenuItemImage * menuItem2 = [CCMenuItemImage itemWithNormalImage:@"findout.png"
+                                                         selectedImage: @"findout.png"
+                                                                target:self
+                                                              selector:@selector(doSomething:)];
+    
+    //add get involved text
+    /*
+    CCLabelTTF* label = [CCLabelTTF labelWithString:@"Go to http://www.gibbonslacklines.com/us/ for more info" fontName:@"Marker Felt" fontSize:12];
+    CGSize size2 = [CCDirector sharedDirector].winSize;
+    label.position = CGPointMake(size2.width * 0.5, size2.height * 0.4);
+    [self addChild:label];
+     */
+    
+    
     menuItem1.tag=1;
+    menuItem2.tag=2;
     
 	// Create a menu and add menu items to it
-	CCMenu * myMenu = [CCMenu menuWithItems:menuItem1, nil];
+	CCMenu * myMenu = [CCMenu menuWithItems:menuItem1,menuItem2, nil];
     
     menuItem1.position = ccp(screenSize.width*.5, screenSize.height*.1);
+    menuItem2.position= ccp(screenSize.width*.5, screenSize.height*.4);
     myMenu.position = ccp(0,0);
     
 	// add the menu to your scene
 	[self addChild:myMenu];
     
-    //add get involved text
-    CCLabelTTF* label = [CCLabelTTF labelWithString:@"Go to http://www.gibbonslacklines.com/us/ for more info" fontName:@"Marker Felt" fontSize:12];
-    CGSize size2 = [CCDirector sharedDirector].winSize;
-    label.position = CGPointMake(size2.width * 0.5, size2.height * 0.4);
-    [self addChild:label];
+    
     
 }
 
@@ -83,8 +95,13 @@
 	int parameter = menuItem.tag;
     
     if (parameter==1) {
+        
         [[CCDirector sharedDirector] replaceScene: [MainMenuLayer scene]];
         
+    }
+    
+    if (parameter==2){
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.gibbonslacklines.com/us/"]];
     }
 }
 
